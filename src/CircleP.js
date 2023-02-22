fcd50d9d8e6f922b8efc94dc6826742a58514552
Mahudoom Naina <66937573+easa2000@users.noreply.github.com>
2023-02-22 08:22:16 +0530

    Update CircleP.js

--- src/CircleP.js
+++ src/CircleP.js
@@ -6,7 +6,7 @@ class CircleP extends React.Component{
         var perm = 2 * 3.14 * radius;
         return(
             <div>
-                <h1>Perimeter of The Shapes (using Class Container)</h1>
+                <h1>Perimeter of The Shapes (using Class Component)</h1>
                 <hr/>
                 <h3>Circle</h3>
                 <p>radius : {radius}</p>
